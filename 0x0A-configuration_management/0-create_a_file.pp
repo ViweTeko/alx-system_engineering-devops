c2a2db27a5e743ca9d6c7d5a682cf1aa624ed260
ViweTeko <iamviwe.teko@gmail.com>
2024-03-22 21:45:18 +0200

    Fixed alignment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Puppet manifest to create a file
 file { '/tmp/school':
-    ensure => present,
+    ensure  => present,
     content => 'I love Puppet',
-    mode   => '0744',
-    owner  => 'www-data',
-    group  => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,15 +5,16 @@ package { 'python3':
 
 # Install flask from pip3
 package { 'flask':
-  ensure  => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
-  before  => Exec['downgrade_werkzeug'],
+  before   => Exec['downgrade_werkzeug'],
 }
 
 # Downgrade Werkzeug to a specific version
 exec { 'downgrade_werkzeug':
   command => 'pip3 install werkzeug==2.1.1',
   path    => '/usr/bin',
+
 # Check's version if 2.1.1 before downgrading
 unless  => 'pip3 show werkzeug | grep -q "Version: 2.1.1"'
 }
