cbc4a4fbf80858ca897f04f7e7bd84d5bd20c499
ViweTeko <iamviwe.teko@gmail.com>
2024-05-07 13:39:35 +0200

    Advanced API

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Contains recurse function"""
+import requests
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot subreddit posts"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent": "0x16-api_advanced:project:\
+                    v1.0.00(by /u/bdov"}
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    results = response.json().get('data')
+    after = results.get('after')
+    count += results.get('dist')
+    for a in results.get('children'):
+        hot_list.append(a.get('data').get('title'))
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
